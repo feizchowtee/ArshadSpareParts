1a7a2f60c502d6cf157aadaf07a0d8d1f3ab8a88
Feiz Chowtee <feiz.chowtee@gmail.com>
2020-04-14 13:46:03 +0400

    Added GSAP Animatations

--- app.js
+++ app.js
@@ -8,3 +8,82 @@ $(document).ready(function(){
 //Rellax.JS
 
 
+var controller = new ScrollMagic.Controller();
+
+$('h1').each(function(i){
+
+    var titles = $(this);
+
+    var animateTitles = new TimelineMax();
+    animateTitles
+    .from(titles, 0.5, { skewX: 10, autoAlpha: 0, y: 30 });
+
+
+
+   new ScrollMagic.Scene({
+       triggerElement : this,
+       triggerHook: 0.8
+       
+   })
+   .setTween(animateTitles).addTo(controller);
+
+});
+
+
+$('h5').each(function(i){
+
+    var subTitles = $(this);
+
+    var animateTitles = new TimelineMax();
+    animateTitles
+    .from(subTitles, 0.5, { skewX: -10, autoAlpha: 0, y: 40 });
+
+
+
+   new ScrollMagic.Scene({
+       triggerElement : this,
+       triggerHook: 0.8
+       
+   })
+   .setTween(animateTitles).addTo(controller);
+
+});
+
+
+$('.list-item').each(function(i){
+
+    var listItem = $(this);
+
+    var animateTitles = new TimelineMax();
+    animateTitles
+    .from(listItem, 0.5, { skewX: 10, autoAlpha: 0, y: 30, stagger: 0.4 });
+
+
+
+   new ScrollMagic.Scene({
+       triggerElement : this,
+       triggerHook: 0.8
+       
+   })
+   .setTween(animateTitles).addTo(controller);
+
+});
+$('.product').each(function(i){
+
+    var product = $(this);
+
+    var animateTitles = new TimelineMax();
+    animateTitles
+    .from(product, 0.5, { skewX: 10, autoAlpha: 0, y: 30, stagger: 0.4 });
+
+
+
+   new ScrollMagic.Scene({
+       triggerElement : this,
+       triggerHook: 0.8
+       
+   })
+   .setTween(animateTitles).addTo(controller);
+
+});
+
